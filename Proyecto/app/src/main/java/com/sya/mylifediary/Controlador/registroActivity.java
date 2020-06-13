d9ffd286fa63a3240a47be7ad50d6be3ed7bbe2d
EALaura <lslhais@gmail.com>
2020-06-13 13:38:06 -0500

    Se desarrolla la interfaz de registro.

---
+++ Proyecto/app/src/main/java/com/sya/mylifediary/Controlador/registroActivity.java
@@ -0,0 +1,15 @@
+package com.sya.mylifediary.Controlador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.sya.mylifediary.R;
+
+public class registroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
