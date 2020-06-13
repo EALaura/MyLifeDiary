1c24b7a757fca37fc4a0314493067194e8db8ed6
EALaura <lslhais@gmail.com>
2020-06-13 11:17:01 -0500

    Creacion de Packages: Modelo y Controlador

--- Proyecto/app/src/main/java/com/sya/mylifediary/Controlador/MainActivity.java
+++ Proyecto/app/src/main/java/com/sya/mylifediary/Controlador/MainActivity.java
@@ -1,8 +1,10 @@
-package com.sya.mylifediary;
+package com.sya.mylifediary.Controlador;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.sya.mylifediary.R;
 
 public class MainActivity extends AppCompatActivity {
 
