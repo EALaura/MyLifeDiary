460d46ea37db4f08d254cb90736b709d29d43255
EALaura <lslhais@gmail.com>
2020-06-15 16:39:54 -0500

    Se implementa dentro de los activitys las interfaces

--- Proyecto/app/src/main/java/com/sya/mylifediary/Controlador/MainActivity.java
+++ Proyecto/app/src/main/java/com/sya/mylifediary/Controlador/MainActivity.java
@@ -2,15 +2,57 @@ package com.sya.mylifediary.Controlador;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.sya.mylifediary.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Botones
+    Button btn_ini_ses;
+    Button btn_salir;
+    Button btn_regi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Ir al activity principal
+        btn_ini_ses = (Button)findViewById(R.id.btn_Ini);
+
+        btn_ini_ses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent principal = new Intent(MainActivity.this, principalActivity.class);
+                startActivity(principal);
+            }
+        });
+
+        //Salir de la aplicacion
+        btn_salir = (Button)findViewById(R.id.btn_Sal);
+
+        btn_salir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        //Ir a registro
+        btn_regi = (Button)findViewById(R.id.btn_Reg);
+
+        btn_regi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registro = new Intent(MainActivity.this, registroActivity.class);
+                startActivity(registro);
+            }
+        });
+
     }
 }
--- Proyecto/app/src/main/java/com/sya/mylifediary/Controlador/registroActivity.java
+++ Proyecto/app/src/main/java/com/sya/mylifediary/Controlador/registroActivity.java
@@ -2,14 +2,41 @@ package com.sya.mylifediary.Controlador;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import com.sya.mylifediary.R;
 
 public class registroActivity extends AppCompatActivity {
+    //Botones
+    Button btn_regis;
+    Button btn_can;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        //Registrarse en la base de datos
+        btn_regis = (Button)findViewById(R.id.btn_regi);
+        btn_regis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText("Su Usuario esta registrandose, vuelva a iniciar Sesión",);
+            }
+        });
+
+        //Boton Cancelar redirige a la pantalla de Inicio
+        btn_can = (Button)findViewById(R.id.btn_can);
+        btn_can.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login = new Intent(registroActivity.this, MainActivity.class);
+                startActivity(login);
+            }
+        });
     }
 }
