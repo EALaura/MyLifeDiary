7c4be2c85071ff5f20cc4dfcb6a60a1de6008b11
MarysolPL <mperezl@unsa.edu.pe>
2020-06-16 06:08:02 -0500

    Creacion de modelos de Historia y persona

---
+++ Proyecto/app/src/main/java/com/sya/mylifediary/Model/History.java
@@ -0,0 +1,31 @@
+package com.sya.mylifediary.Model;
+
+public class History {
+    float latitude;
+    float longitude;
+    String description;
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ Proyecto/app/src/main/java/com/sya/mylifediary/Model/Person.java
@@ -0,0 +1,40 @@
+package com.sya.mylifediary.Model;
+
+public class Person {
+    String name;
+    String email;
+    String user;
+    String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
