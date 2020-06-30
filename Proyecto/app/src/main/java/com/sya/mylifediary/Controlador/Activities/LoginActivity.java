7de9dfbe6218b6267db22c392ce56e221880521e
EALaura <lslhais@gmail.com>
2020-06-29 19:55:06 -0500

    Actualización de Interfaces en Formato Horizontal

--- Proyecto/app/src/main/java/com/sya/mylifediary/Controlador/Activities/LoginActivity.java
+++ Proyecto/app/src/main/java/com/sya/mylifediary/Controlador/Activities/LoginActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
@@ -26,6 +28,8 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
 
         bindUI();
