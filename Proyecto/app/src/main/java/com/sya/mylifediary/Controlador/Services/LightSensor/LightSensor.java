f70fad8b6cb6f1f4615c08fafc823909c1fcab09
EALaura <lslhais@gmail.com>
2020-08-01 12:54:06 -0500

    Crear la Clase LightSensor.java en el controlador

---
+++ Proyecto/app/src/main/java/com/sya/mylifediary/Controlador/Services/LightSensor/LightSensor.java
@@ -0,0 +1,49 @@
+package com.sya.mylifediary.Controlador.Services.LightSensor;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.widget.LinearLayout;
+
+public class LightSensor implements SensorEventListener {
+    private Context context;
+    private SensorManager sensorManager;
+    private Sensor sensorLuz;
+    private LinearLayout linearLayout;
+
+    //Constructor de light sensor recibe el contexto y el layout a modificar.
+    public LightSensor(Context context, LinearLayout linearLayout) {
+        this.context = context;
+        this.linearLayout = linearLayout;
+        iniciarSensor();
+    }
+
+    // Inicia el Sensor de Luz
+    private void iniciarSensor() {
+        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        sensorLuz = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        sensorManager.registerListener(this, sensorLuz, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_LIGHT) {
+            //Menor Luz
+            if (sensorEvent.values[0] < 20) {
+                linearLayout.setBackgroundColor(Color.BLUE); // a menor luz
+            } else {
+                linearLayout.setBackgroundColor(Color.RED); // a mayor luz
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
