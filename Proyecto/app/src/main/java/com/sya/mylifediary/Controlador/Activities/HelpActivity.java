8d9b5d8ed1b0a10cd475b64b5f31c35f5f2ce5fb
EALaura <lslhais@gmail.com>
2020-08-01 23:14:22 -0500

    Se agrega los sensores de LightSensor y Acelerometro

--- Proyecto/app/src/main/java/com/sya/mylifediary/Controlador/Activities/HelpActivity.java
+++ Proyecto/app/src/main/java/com/sya/mylifediary/Controlador/Activities/HelpActivity.java
@@ -2,15 +2,48 @@ package com.sya.mylifediary.Controlador.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
+import com.sya.mylifediary.Controlador.Services.Acelerometro.Acelerometro;
+import com.sya.mylifediary.Controlador.Services.LightSensor.LightSensor;
 import com.sya.mylifediary.R;
 
 public class HelpActivity extends AppCompatActivity {
+    private LinearLayout helpView;
+    private SharedPreferences sharedPreferences;
+    private Acelerometro acelerometro;
+    private LightSensor lightSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        sharedPreferences = getSharedPreferences("Preferences", Context.MODE_PRIVATE);
+        helpView = findViewById(R.id.helpView);
+
+        // Configuracion de Sensores
+        acelerometro = new Acelerometro(this, sharedPreferences);
+        lightSensor = new LightSensor(this, helpView);
+    }
+
+    // Cuando la activity esta en background se detienen las
+    // lecturas del acelerometro y light sensor
+    @Override
+    protected void onPause() {
+        acelerometro.getSensorManager().unregisterListener(acelerometro);
+        lightSensor.getSensorManager().unregisterListener(lightSensor);
+        super.onPause();
+    }
+
+    // Cuando el activity se retoma se retoman las lecturas
+    @Override
+    protected void onRestart() {
+        acelerometro.iniciarSensor();
+        lightSensor.iniciarSensor();
+        super.onRestart();
     }
 }
